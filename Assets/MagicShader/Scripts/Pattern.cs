7cd9446c1f87a2059f9cce43ca93edfa0e714930
IIRedDataII <julian.z@gmx.net>
2021-12-05 23:04:15 +0100

    Fixed Pattern Textures

--- Assets/MagicShader/Scripts/Pattern.cs
+++ Assets/MagicShader/Scripts/Pattern.cs
@@ -4,7 +4,7 @@ public class Pattern : MonoBehaviour
 {
     
     private const float Delay = 1f / 100f;
-    private const int Step = 10;
+    private const int Step = 1;
     
     [SerializeField] private Material material;
     [SerializeField] private Texture2D pattern;
